e61e7e771b9a36bb7e51ab58ad005c38556c3992
Timóteo Stifft <timoteostifft@gmail.com>
2022-04-06 10:02:21 -0300

    feature: set node server autoreload

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
-import express from 'express';
+import express, { request, response } from 'express';
 
 const app = express();
 
 app.get("/", (request, response) => {
-  return response.json({message: "Hello!"})
+  return response.json({message: 'Hello!'})
 })
 
-app.listen(3333)
+app.listen(3333, () => {console.log("Server is Running!")})
