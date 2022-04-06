a5ffd158cfad2cf914a274f2b6f7e15d29f32ab0
Timóteo Stifft <timoteostifft@gmail.com>
2022-04-06 02:04:13 -0300

    chore: add typescript dependecies

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,9 @@
-import express from 'express';
+import express, { response } from 'express';
 
 const app = express();
 
-app.listen()
+app.get("/", (request, response) => {
+  return response.json({message: "Hello!"})
+})
+
+app.listen(3333)
