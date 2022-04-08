ac938876e0f53939ef9960a92c999f24fb370787
Timóteo Stifft <timoteostifft@gmail.com>
2022-04-08 10:08:50 -0300

    refactor: correct imports

--- src/modules/cars/services/CreateCategoryService.ts
+++ src/modules/cars/services/CreateCategoryService.ts
@@ -3,7 +3,7 @@ import { ICategoriesRepository } from '../repositories/ICategoriesRepository';
 interface IRequest {
   name: string,
   description: string,
-}
+} 
 
 class CreateCategoryService {
   constructor(private categoriesRepository: ICategoriesRepository) {}
