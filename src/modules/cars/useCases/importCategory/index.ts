e4492688d94ffdd50b53798caa2a218a00f38651
Timóteo Stifft <timoteostifft@gmail.com>
2022-04-13 02:22:32 -0300

    chore: create import category methods index

---
+++ src/modules/cars/useCases/importCategory/index.ts
@@ -0,0 +1,7 @@
+import { ImportCategoryController } from "./ImportCategoryController";
+import { ImportCategoryUseCase } from "./ImportCategoryUseCase";
+
+const importCategoryUsecase = new ImportCategoryUseCase();
+const importCategoryController = new ImportCategoryController(importCategoryUsecase);
+
+export { importCategoryController }
