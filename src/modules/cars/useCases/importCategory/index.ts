0f253b6babca5f7679933b852d829b3afb02ca30
Timóteo Stifft <timoteostifft@gmail.com>
2022-04-13 12:11:16 -0300

    chore: add categories repository to import category index

--- src/modules/cars/useCases/importCategory/index.ts
+++ src/modules/cars/useCases/importCategory/index.ts
@@ -1,7 +1,9 @@
+import { CategoriesRepository } from "../../repositories/implementations/CategoriesRepository";
 import { ImportCategoryController } from "./ImportCategoryController";
 import { ImportCategoryUseCase } from "./ImportCategoryUseCase";
 
-const importCategoryUsecase = new ImportCategoryUseCase();
+const categoriesRepository = CategoriesRepository.getInstance()
+const importCategoryUsecase = new ImportCategoryUseCase(categoriesRepository);
 const importCategoryController = new ImportCategoryController(importCategoryUsecase);
 
 export { importCategoryController }
