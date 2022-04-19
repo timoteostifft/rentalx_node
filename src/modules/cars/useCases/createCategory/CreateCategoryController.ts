bb21809c7ac1f9f7b1d5396af87ca04e57624bd0
Timóteo Stifft <timoteostifft@gmail.com>
2022-04-19 10:05:27 -0300

    feature: add tsyringe to create category controller

--- src/modules/cars/useCases/createCategory/CreateCategoryController.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryController.ts
@@ -1,14 +1,15 @@
+import { container } from 'tsyringe';
 import { Response, Request } from 'express';
 import { CreateCategoryUseCase } from './CreateCategoryUseCase';
 
 class CreateCategoryController {
-
-  constructor(private createCategoryUseCase: CreateCategoryUseCase){}
-
-  async handle(request: Request, response: Response): Promise<Response>{
+    async handle(request: Request, response: Response): Promise<Response>{
+    
     const { name, description } = request.body;
 
-    await this.createCategoryUseCase.execute({ name, description });
+    const createCategoryUseCase = container.resolve(CreateCategoryUseCase)
+
+    await createCategoryUseCase.execute({ name, description });
   
     return response.status(201).send();
   }
