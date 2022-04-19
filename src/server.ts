034e665080933ed58cf707b15f574cbc4875388c
Timóteo Stifft <timoteostifft@gmail.com>
2022-04-19 10:04:29 -0300

    chore: add tsirynge imports to server file

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,10 @@
+import "reflect-metadata";
 import express from 'express';
 import { router } from './routes';
 
 import "./database"
+
+import "./shared/container"
 
 const app = express();
 app.use(express.json());
