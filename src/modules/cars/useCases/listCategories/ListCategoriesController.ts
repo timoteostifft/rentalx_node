48eb3028e568eca1e46dcd20c21eeaa037fdf6c3
Timóteo Stifft <timoteostifft@gmail.com>
2022-04-19 12:12:55 -0300

    refactor: update list categories methods with syringe

--- src/modules/cars/useCases/listCategories/ListCategoriesController.ts
+++ src/modules/cars/useCases/listCategories/ListCategoriesController.ts
@@ -1,12 +1,13 @@
 import { Request, Response } from 'express'
+import { container } from 'tsyringe'
+
 import { ListCategoriesUseCase } from './ListCategoriesUseCase';
 
 class ListCategoriesController {
+  async handle(request: Request, response: Response): Promise<Response>{
+    const listCategoriesUseCase = container.resolve(ListCategoriesUseCase)
 
-  constructor(private listCategoriesUseCase: ListCategoriesUseCase){}
-
-  handle(request: Request, response: Response){
-    const all = this.listCategoriesUseCase.execute();
+    const all = await listCategoriesUseCase.execute();
 
     return response.json(all);
   }
