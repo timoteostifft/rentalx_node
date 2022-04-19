9ed1c83908443cb13c24f8693b11a9e52fe24ba1
Timóteo Stifft <timoteostifft@gmail.com>
2022-04-19 12:09:24 -0300

    refactor: remove unused index for dependency injection

--- src/modules/cars/useCases/importCategory/index.ts
+++
@@ -1,9 +0,0 @@
-import { CategoriesRepository } from "../../repositories/implementations/CategoriesRepository";
-import { ImportCategoryController } from "./ImportCategoryController";
-import { ImportCategoryUseCase } from "./ImportCategoryUseCase";
-
-const categoriesRepository = null;
-const importCategoryUsecase = new ImportCategoryUseCase(categoriesRepository);
-const importCategoryController = new ImportCategoryController(importCategoryUsecase);
-
-export { importCategoryController }
