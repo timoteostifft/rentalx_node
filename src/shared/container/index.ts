56ec1ce20323a0eb76aaa03c5d25fa76e89d5d2f
Timóteo Stifft <timoteostifft@gmail.com>
2022-04-19 23:38:50 -0300

    feature: create user repository container

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,9 +1,13 @@
 import { container } from "tsyringe";
 
-import { ICategoriesRepository } from '../../modules/cars/repositories/ICategoriesRepository'
 import { CategoriesRepository } from "../../modules/cars/repositories/implementations/CategoriesRepository";
+import { ICategoriesRepository } from '../../modules/cars/repositories/ICategoriesRepository'
+
 import { SpecificationsRepository } from "../../modules/cars/repositories/implementations/SpecificationsRepository";
 import { ISpecificationsRepository } from "../../modules/cars/repositories/ISpecificationsRepository";
+
+import { UsersRepository } from "../../modules/accounts/repositories/implementations/UsersRepository";
+import { IUserRepository } from "../../modules/accounts/repositories/IUsersRepository";
 
 container.registerSingleton<ICategoriesRepository>(
   "CategoriesRepository",
@@ -13,4 +17,9 @@ container.registerSingleton<ICategoriesRepository>(
 container.registerSingleton<ISpecificationsRepository>(
   "SpecificationsRepository",
   SpecificationsRepository
+)
+
+container.registerSingleton<IUserRepository>(
+  "UsersRepository",
+  UsersRepository
 )
