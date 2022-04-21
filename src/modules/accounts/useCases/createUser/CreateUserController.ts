f84ef7bd6589ebffcaa9386fe9729420429ef9cc
Timóteo Stifft <timoteostifft@gmail.com>
2022-04-21 04:33:54 -0300

    refactor: remove username attribute from user

--- src/modules/accounts/useCases/createUser/CreateUserController.ts
+++ src/modules/accounts/useCases/createUser/CreateUserController.ts
@@ -4,13 +4,12 @@ import { CreateUserUseCase } from './CreateUserUseCase'
 
 class CreateUserController {
   async handle(request: Request, reponse: Response): Promise<Response>{
-    const { name, username, password, driver_license, email} = request.body
+    const { name, password, driver_license, email} = request.body
 
     const createUserUseCase = container.resolve(CreateUserUseCase)
 
     await createUserUseCase.execute({
       name,
-      username,
       password,
       email,
       driver_license
