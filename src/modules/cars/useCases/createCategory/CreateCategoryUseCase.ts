4406ca6359418399f302b704a6322a01d17f3095
Timóteo Stifft <timoteostifft@gmail.com>
2022-04-25 07:47:15 -0300

    chore: apply custom error class usage

--- src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
@@ -1,4 +1,5 @@
 import { inject, injectable } from 'tsyringe';
+import { AppError } from '../../../../errors/AppError';
 
 import { ICategoriesRepository } from '../../repositories/ICategoriesRepository';
 
