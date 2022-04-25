11aedf3d7b7fa7d12884b2ecb23c6d85487714b8
Timóteo Stifft <timoteostifft@gmail.com>
2022-04-25 07:55:20 -0300

    feature: create custom base error message for server usage

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,27 @@
 import "reflect-metadata";
-import express from 'express';
+import express, { Request, Response, NextFunction } from "express"
 import { router } from './routes';
 
 import "./database"
 
 import "./shared/container"
+import { AppError } from "./errors/AppError";
 
 const app = express();
 app.use(express.json());
 app.use(router);
+
+app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
+  if(err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      message: err.message
+    })
+  }
+
+  return response.status(500).json({
+    status: "error",
+    message: `Internal server error - ${err.message}`
+  })
+})
+
 app.listen(3333, () => {console.log("Server is Running!")});
