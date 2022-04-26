8df722298593b7a35fcc302f73e1388d6dfbc80c
Timóteo Stifft <timoteostifft@gmail.com>
2022-04-26 02:53:49 -0300

    refactor: add user id and avatar atributtes to patch user avatar

--- src/modules/accounts/repositories/implementations/UsersRepository.ts
+++ src/modules/accounts/repositories/implementations/UsersRepository.ts
@@ -10,13 +10,15 @@ class UsersRepository implements IUserRepository{
     this.repository = getRepository(User)
   }
   
-  async create({name, email, driver_license, password}: ICreateUserDTO): Promise<void> {
+  async create({name, email, driver_license, password, avatar, id}: ICreateUserDTO): Promise<void> {
     
     const user = this.repository.create({
       name,
       email,
       driver_license,
-      password
+      password,
+      avatar,
+      id,
     })
 
     await this.repository.save(user)
