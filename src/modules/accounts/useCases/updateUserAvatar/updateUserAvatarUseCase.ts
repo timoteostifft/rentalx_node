6027a358b949c70662d6c380d225c8d31dc67aa7
Timóteo Stifft <timoteostifft@gmail.com>
2022-04-26 09:42:33 -0300

    feature: add delete file if exists method on update user avatar controller

--- src/modules/accounts/useCases/updateUserAvatar/updateUserAvatarUseCase.ts
+++ src/modules/accounts/useCases/updateUserAvatar/updateUserAvatarUseCase.ts
@@ -1,4 +1,7 @@
 import { inject, injectable } from "tsyringe";
+
+import { deleteFile } from "../../../../utils/file";
+
 import { IUserRepository } from "../../repositories/IUsersRepository";
 
 interface IRequest {
@@ -15,6 +18,10 @@ class UpdateUserAvatarUseCase {
 
   async execute({ user_id, avatar_file }: IRequest): Promise<void>{
     const user = await this.usersRepository.findById(user_id);
+
+    if (user.avatar){
+      await deleteFile(`./tmp/avatar/${user.avatar}`)
+    };
 
     user.avatar = avatar_file;
 
