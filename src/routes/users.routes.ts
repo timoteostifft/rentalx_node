8d27648fd7649353e5b01032bda68133db92877d
Timóteo Stifft <timoteostifft@gmail.com>
2022-04-26 02:55:31 -0300

    refactor: add ensure authenticated middleware and multer upload to update user avatar patch route

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -4,17 +4,18 @@ import multer from 'multer';
 import { CreateUserController } from '../modules/accounts/useCases/createUser/CreateUserController';
 import { UpdateUserAvatarController } from '../modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController';
 
+import uploadConfig from "../config/upload"
+import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
+
 const usersRoutes = Router();
 
-const upload = multer({
-  dest: "avatar"
-})
+const uploadAvatar = multer(uploadConfig.upload('./tmp/avatar'))
 
 const createUserController = new CreateUserController();
 const updateUserAvatarController = new UpdateUserAvatarController();
 
 usersRoutes.post("/", createUserController.handle);
 
-usersRoutes.patch("/avatar", upload.single("file"), updateUserAvatarController.handle);
+usersRoutes.patch("/avatar", ensureAuthenticated, uploadAvatar.single("avatar"), updateUserAvatarController.handle);
 
 export { usersRoutes }
