ff7e8f5d97edcf324ee73cf9a61561a20131a02e
Timóteo Stifft <timoteostifft@gmail.com>
2022-04-29 09:08:01 -0300

    refactor: update imports

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,13 +1,13 @@
 import { container } from "tsyringe";
 
-import { CategoriesRepository } from "../../modules/cars/repositories/implementations/CategoriesRepository";
-import { ICategoriesRepository } from '../../modules/cars/repositories/ICategoriesRepository'
+import { CategoriesRepository } from "@modules/cars/repositories/implementations/CategoriesRepository";
+import { ICategoriesRepository } from '@modules/cars/repositories/ICategoriesRepository'
 
-import { SpecificationsRepository } from "../../modules/cars/repositories/implementations/SpecificationsRepository";
-import { ISpecificationsRepository } from "../../modules/cars/repositories/ISpecificationsRepository";
+import { SpecificationsRepository } from "@modules/cars/repositories/implementations/SpecificationsRepository";
+import { ISpecificationsRepository } from "@modules/cars/repositories/ISpecificationsRepository";
 
-import { UsersRepository } from "../../modules/accounts/repositories/implementations/UsersRepository";
-import { IUserRepository } from "../../modules/accounts/repositories/IUsersRepository";
+import { UsersRepository } from "@modules/accounts/repositories/implementations/UsersRepository";
+import { IUserRepository } from "@modules/accounts/repositories/IUsersRepository";
 
 container.registerSingleton<ICategoriesRepository>(
   "CategoriesRepository",
