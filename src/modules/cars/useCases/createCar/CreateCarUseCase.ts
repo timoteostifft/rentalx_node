c44464969786fa30d9e7fd9cb7bb2535c189efbf
Timóteo Stifft <timoteostifft@gmail.com>
2022-05-02 17:06:22 -0300

    feature: create create car use case

---
+++ src/modules/cars/useCases/createCar/CreateCarUseCase.ts
@@ -0,0 +1,27 @@
+import { ICarsRepository } from "@modules/cars/repositories/ICarsRepository";
+import { inject, injectable } from "tsyringe";
+
+interface IRequest {
+  name: string;
+  description: string;
+  daily_rate: number;
+  license_plate: string;
+  fine_amount: number;
+  brand: string;
+  category_id: string;
+}
+
+@injectable()
+class CreateCarUseCase {
+
+  constructor(
+    @inject("CarsRepository")
+    private carsRepository: ICarsRepository
+  ) {}
+
+  async execute({name, description, brand, category_id, daily_rate, fine_amount, license_plate} : IRequest): Promise<void> {
+    this.carsRepository.create({name, description, brand, category_id, daily_rate, fine_amount, license_plate})
+  }
+}
+
+export { CreateCarUseCase }
