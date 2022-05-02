39f8e131f6c8dc5a8a4b2ead4ec0b805d1a8ddf8
Timóteo Stifft <timoteostifft@gmail.com>
2022-05-01 22:17:47 -0300

    refactor: update imports

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,12 +1,12 @@
 import { container } from "tsyringe";
 
-import { CategoriesRepository } from "@modules/cars/repositories/implementations/CategoriesRepository";
+import { CategoriesRepository } from "@modules/cars/infra/typeorm/repositories/CategoriesRepository";
 import { ICategoriesRepository } from '@modules/cars/repositories/ICategoriesRepository'
 
-import { SpecificationsRepository } from "@modules/cars/repositories/implementations/SpecificationsRepository";
+import { SpecificationsRepository } from "@modules/cars/infra/typeorm/repositories/SpecificationsRepository";
 import { ISpecificationsRepository } from "@modules/cars/repositories/ISpecificationsRepository";
 
-import { UsersRepository } from "@modules/accounts/repositories/implementations/UsersRepository";
+import { UsersRepository } from "@modules/accounts/infra/typeorm/repositories/UsersRepository";
 import { IUserRepository } from "@modules/accounts/repositories/IUsersRepository";
 
 container.registerSingleton<ICategoriesRepository>(
