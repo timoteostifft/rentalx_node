7c8de9e4cc7a73a02f80a7b5cf63fca16b779fbd
Timóteo Stifft <timoteostifft@gmail.com>
2022-05-01 22:16:25 -0300

    refactor: update specification repository folder

--- src/modules/cars/infra/typeorm/repositories/SpecificationsRepository.ts
+++ src/modules/cars/infra/typeorm/repositories/SpecificationsRepository.ts
@@ -1,6 +1,6 @@
 import { getRepository, Repository } from "typeorm";
-import { Specification } from "../../entities/Specification";
-import { ISpecificationsRepository, ICreateSpecificationDTO } from "../ISpecificationsRepository";
+import { Specification } from "../entities/Specification";
+import { ISpecificationsRepository, ICreateSpecificationDTO } from "../../../repositories/ISpecificationsRepository";
 
 class SpecificationsRepository implements ISpecificationsRepository{
   private repository: Repository<Specification>;
