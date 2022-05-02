4be2f027699611204b10a199962d42b5a6e22022
Timóteo Stifft <timoteostifft@gmail.com>
2022-05-01 23:04:15 -0300

    refactor: update database folder to typeorm and set new imports

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -2,7 +2,7 @@ import "reflect-metadata";
 import express, { Request, Response, NextFunction } from "express"
 import "express-async-errors"
 
-import "../../../database"
+import "../typeorm"
 
 import "@shared/container"
 
