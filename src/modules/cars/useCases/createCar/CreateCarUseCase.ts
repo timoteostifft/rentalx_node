c7b64967eb5ded5223b64105d3948de8a95553ca
Timóteo Stifft <timoteostifft@gmail.com>
2022-05-03 11:08:10 -0300

    feature: create check if car exists before create method on create car use case

--- src/modules/cars/useCases/createCar/CreateCarUseCase.ts
+++ src/modules/cars/useCases/createCar/CreateCarUseCase.ts
@@ -1,4 +1,6 @@
+import { Car } from "@modules/cars/infra/typeorm/entities/Car";
 import { ICarsRepository } from "@modules/cars/repositories/ICarsRepository";
+import { AppError } from "@shared/errors/AppError";
 import { inject, injectable } from "tsyringe";
 
 interface IRequest {
@@ -11,16 +13,32 @@ interface IRequest {
   category_id: string;
 }
 
-@injectable()
+// @injectable()
 class CreateCarUseCase {
 
   constructor(
-    @inject("CarsRepository")
+    // @inject("CarsRepository")
     private carsRepository: ICarsRepository
   ) {}
 
-  async execute({name, description, brand, category_id, daily_rate, fine_amount, license_plate} : IRequest): Promise<void> {
-    this.carsRepository.create({name, description, brand, category_id, daily_rate, fine_amount, license_plate})
+  async execute({name, description, brand, category_id, daily_rate, fine_amount, license_plate} : IRequest): Promise<Car> {
+
+    const carAlreadyExists = await this.carsRepository.findByLicensePlate(license_plate);
+
+    if(carAlreadyExists){
+      throw new AppError("Car already exists!");
+    }
+
+    const car = await this.carsRepository.create({
+      name,
+      description,
+      brand,
+      category_id,
+      daily_rate,
+      fine_amount,
+      license_plate});
+
+      return car;
   }
 }
 
