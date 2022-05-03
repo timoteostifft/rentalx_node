a5eb8cf5aa96809f34acc9602acc25f4dbf78826
Timóteo Stifft <timoteostifft@gmail.com>
2022-05-03 11:07:33 -0300

    feature: create create car tests

--- src/modules/cars/useCases/createCar/CreateCarUseCase.spec.ts
+++ src/modules/cars/useCases/createCar/CreateCarUseCase.spec.ts
@@ -1,4 +1,5 @@
 import { CarsRepositoryInMemory } from "@modules/cars/repositories/in-memory/CarsRepositoryInMemory";
+import { AppError } from "@shared/errors/AppError";
 import { CreateCarUseCase } from "./CreateCarUseCase"
 
 let createCarUseCase: CreateCarUseCase;
@@ -12,7 +13,7 @@ describe("Create Car", () => {
   })
 
   it("Should be able to create a new car", async () => {
-    await createCarUseCase.execute({
+    const car = await createCarUseCase.execute({
       name: "Name Car",
       description: "Description Car",
       brand: "Brand",
@@ -21,5 +22,45 @@ describe("Create Car", () => {
       license_plate: "ABC-1234",
       category_id: "category"
     });
+
+    expect(car).toHaveProperty("id");
+  })
+
+  it("Should not be able to create a car if with exists license plate", () => {
+    expect(async () => {
+      await createCarUseCase.execute({
+        name: "Name Car1",
+        description: "Description Car",
+        brand: "Brand",
+        daily_rate: 100, 
+        fine_amount: 60, 
+        license_plate: "ABC-1234",
+        category_id: "category"
+      });
+
+      await createCarUseCase.execute({
+        name: "Name Car2",
+        description: "Description Car",
+        brand: "Brand",
+        daily_rate: 100, 
+        fine_amount: 60, 
+        license_plate: "ABC-1234",
+        category_id: "category"
+      });
+    }).rejects.toBeInstanceOf(AppError);
+  })
+
+  it("Should be able to create a car if with available true by default", async () => {
+    const car = await createCarUseCase.execute({
+      name: "Name Car Available",
+      description: "Description Car",
+      brand: "Brand",
+      daily_rate: 100, 
+      fine_amount: 60, 
+      license_plate: "ABCD-1234",
+      category_id: "category"
+    });
+
+    expect(car.available).toBe(true);
   })
 })
