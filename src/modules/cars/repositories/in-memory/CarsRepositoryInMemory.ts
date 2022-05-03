80f65b476bb425ac4f4afa79c834831ba2009ac2
Timóteo Stifft <timoteostifft@gmail.com>
2022-05-03 11:07:10 -0300

    feature: create find by license plate method on cars repository

--- src/modules/cars/repositories/in-memory/CarsRepositoryInMemory.ts
+++ src/modules/cars/repositories/in-memory/CarsRepositoryInMemory.ts
@@ -5,7 +5,7 @@ import { ICarsRepository } from "../ICarsRepository";
 class CarsRepositoryInMemory implements ICarsRepository {
 
   cars: Car[] = [];
-  async create({ brand, category_id, daily_rate, description, fine_amount, license_plate, name }: ICreateCarDTO): Promise<void> {
+  async create({ brand, category_id, daily_rate, description, fine_amount, license_plate, name }: ICreateCarDTO): Promise<Car> {
     const car = new Car();
 
     Object.assign(car, {
@@ -19,6 +19,12 @@ class CarsRepositoryInMemory implements ICarsRepository {
     });
 
     this.cars.push(car);
+
+    return car;
+  }
+
+  async findByLicensePlate(license_plate: string): Promise<Car> {
+    return this.cars.find((car) => car.license_plate === license_plate);
   }
 }
 
