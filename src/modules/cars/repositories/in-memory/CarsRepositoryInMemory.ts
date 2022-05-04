bddb1018a38b5e9f20163d1fe9f1013752c76980
Timóteo Stifft <timoteostifft@gmail.com>
2022-05-04 12:02:52 -0300

    feature: add find available method in cars repository in memory

--- src/modules/cars/repositories/in-memory/CarsRepositoryInMemory.ts
+++ src/modules/cars/repositories/in-memory/CarsRepositoryInMemory.ts
@@ -3,8 +3,9 @@ import { Car } from "@modules/cars/infra/typeorm/entities/Car";
 import { ICarsRepository } from "../ICarsRepository";
 
 class CarsRepositoryInMemory implements ICarsRepository {
-
+ 
   cars: Car[] = [];
+
   async create({ brand, category_id, daily_rate, description, fine_amount, license_plate, name }: ICreateCarDTO): Promise<Car> {
     const car = new Car();
 
@@ -25,6 +26,11 @@ class CarsRepositoryInMemory implements ICarsRepository {
 
   async findByLicensePlate(license_plate: string): Promise<Car> {
     return this.cars.find((car) => car.license_plate === license_plate);
+  }
+
+  async findAvailable(): Promise<Car[]> {
+    const cars = this.cars.filter((car) => {car.available === true});
+    return cars;
   }
 }
 
