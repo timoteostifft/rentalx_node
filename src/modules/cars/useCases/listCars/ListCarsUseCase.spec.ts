2ea2fc6e815ab9b47423f414340f3eda5cf98ba3
Timóteo Stifft <timoteostifft@gmail.com>
2022-05-04 12:03:12 -0300

    feature: create list cars use case spec test

---
+++ src/modules/cars/useCases/listCars/ListCarsUseCase.spec.ts
@@ -0,0 +1,30 @@
+import { CarsRepositoryInMemory } from "@modules/cars/repositories/in-memory/CarsRepositoryInMemory";
+import { ListCarsUseCase } from "./ListCarsUseCase"
+
+let listCarsUseCase: ListCarsUseCase;
+let carsRepositoryInMemory: CarsRepositoryInMemory;
+
+describe("List Cars", () => {
+
+  beforeEach(() => {
+    carsRepositoryInMemory = new CarsRepositoryInMemory();
+    listCarsUseCase = new ListCarsUseCase(carsRepositoryInMemory);
+  })
+
+  it("Should be able to list all available cars", async () => {
+
+    const car = await carsRepositoryInMemory.create({
+      "name": "Car1",
+      "description": "Car description",
+      "daily_rate": 140,
+      "license_plate": "DEF-1212",
+      "fine_amount": 180,
+      "brand": "Car brand",
+      "category_id": "Category ID"
+    })
+
+    const cars = await listCarsUseCase.execute();
+    
+    expect(cars).toEqual([car]);
+  })
+})
