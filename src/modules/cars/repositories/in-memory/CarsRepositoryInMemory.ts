c16bb6b24cef79761b1034c55fa18e81798e2702
Timóteo Stifft <timoteostifft@gmail.com>
2022-05-06 15:38:21 -0300

    feature: create find by id cars repository method

--- src/modules/cars/repositories/in-memory/CarsRepositoryInMemory.ts
+++ src/modules/cars/repositories/in-memory/CarsRepositoryInMemory.ts
@@ -3,7 +3,7 @@ import { Car } from "@modules/cars/infra/typeorm/entities/Car";
 import { ICarsRepository } from "../ICarsRepository";
 
 class CarsRepositoryInMemory implements ICarsRepository {
- 
+  
   cars: Car[] = [];
 
   async create({ brand, category_id, daily_rate, description, fine_amount, license_plate, name }: ICreateCarDTO): Promise<Car> {
@@ -41,6 +41,10 @@ class CarsRepositoryInMemory implements ICarsRepository {
       return null;
     })
     return cars;
+  }
+
+  async findById(id: string): Promise<Car> {
+    return this.cars.find((car) => car.id === id);
   }
 }
 
