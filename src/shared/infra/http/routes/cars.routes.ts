fe4f95017db29b9e7f8e8f8eff32b35709d82cca
Timóteo Stifft <timoteostifft@gmail.com>
2022-05-09 20:45:58 -0300

    feature: create create car specification route

--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -3,14 +3,18 @@ import { CreateCarController } from "@modules/cars/useCases/createCar/CreateCarC
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 import { ListAvailableCarsController } from "@modules/cars/useCases/listAvailableCars/listAvailableCarsController";
 import { ensureAdmin } from "../middlewares/ensureAdmin";
+import { CreateCarSpecificationController } from "@modules/cars/useCases/createCarSpecification/CreateCarSpecificationController";
 
 const carsRoutes = Router();
 
 const createCarController = new CreateCarController();
 const listAvailableCarsController = new ListAvailableCarsController();
+const createCarSpecificationController = new CreateCarSpecificationController();
 
 carsRoutes.post("/", ensureAuthenticated, ensureAdmin, createCarController.handle);
 
 carsRoutes.get("/available", listAvailableCarsController.handle);
+
+carsRoutes.post("/specifications/:id", ensureAuthenticated, ensureAdmin, createCarSpecificationController.handle);
 
 export { carsRoutes };
