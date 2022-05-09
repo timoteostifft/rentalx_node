c4a3d9dfcb473cd9955ceb1e891ea9b3bf6feb2c
Timóteo Stifft <timoteostifft@gmail.com>
2022-05-09 18:23:20 -0300

    feature: create find by id method

--- src/modules/cars/infra/typeorm/repositories/CarsRepository.ts
+++ src/modules/cars/infra/typeorm/repositories/CarsRepository.ts
@@ -11,7 +11,7 @@ class CarsRepository implements ICarsRepository {
     this.repository = getRepository(Car);
   }
   
-  async create({ brand, category_id, daily_rate, description, fine_amount, license_plate, name }: ICreateCarDTO): Promise<Car> {
+  async create({ brand, category_id, daily_rate, description, fine_amount, license_plate, name, specifications, id }: ICreateCarDTO): Promise<Car> {
     const car = await this.repository.create({
       brand,
       daily_rate,
@@ -19,7 +19,9 @@ class CarsRepository implements ICarsRepository {
       fine_amount,
       license_plate,
       name,
-      category_id
+      category_id,
+      specifications,
+      id
     });
 
     await this.repository.save(car);
@@ -54,6 +56,13 @@ class CarsRepository implements ICarsRepository {
     const cars = await carsQuery.getMany();
 
     return cars;
+  }
+
+  async findById(id: string): Promise<Car> {
+    const car = await this.repository.findOne({
+      id
+    })
+    return car;
   }
   
 }
